10af833b693375c1ab4514bf2dd6c98532964d29
Ebubekir Sezer <ebubekirssezer@gmail.com>
2022-06-28 21:15:58 +0300

    Initially Created

---
+++ Weekit/Weekit/WeekitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeekitApp.swift
+//  Weekit
+//
+//  Created by EbubekirSezer on 28.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeekitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
